169fea203e88ce6d73b307cfbbe32aaecdfc0ae6
Елисей Ильин <jiraiya106@gmail.com>
2021-05-07 17:32:32 +0300

    jenkins

--- 2/second.groovy
+++ 2/second.groovy
@@ -16,12 +16,13 @@ pipeline {
         stage('Docker') { 
             steps { 
                 sh 'cd ${WORKSPACE} && echo FROM nginx > ${WORKSPACE}/Dockerfile &&'+
-                "echo COPY `find ${WORKSPACE}/release -type f` /var/www/html >> ${WORKSPACE}/Dockerfile"
-              // script{
+                "echo COPY `find ${WORKSPACE}/First/code -type f` /var/www/html >> ${WORKSPACE}/Dockerfile"
+              script{
+                def app = docker.build("test")
               //   sh 'env'
               //   echo "Message: ${params.message}, Some data: ${params.someData}, build number: #${env.BUILD_NUMBER}"
               //   env.setProperty('returnData', "Some data from second job, ${env.BUILD_URL}" )
-              // }
+              }
             }
         }
     }
