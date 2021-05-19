62a39bdc98984b2fbc8d8fc6649d694038eaa697
Елисей Ильин <jiraiya106@gmail.com>
2021-05-19 16:58:52 +0300

    Terraform

---
+++ Terraform/examples/vpc/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+    region  = "eu-central-1"
+    access_key = "AKIAWQ7UOWREDY3NQPNL"
+    secret_key = "8Xks4ZKxmIzIeS8NTjpKJUIz1xKzS+rmgHQcK2Op"
+}
+
+module "vpc" {
+    source                              = "../../modules/vpc"
+    name = "Elisey-VPC"
+    environment = "PROD"
+    # VPC
+    instance_tenancy                    = "dedicated"
+    enable_dns_support                  = "true"
+    enable_dns_hostnames                = "true"
+    assign_generated_ipv6_cidr_block    = "false"
+    enable_classiclink                  = "false"
+    vpc_cidr                            = "10.0.0.0/16"
+    private_subnet_cidrs                = ["10.0.3.0/24"]
+    public_subnet_cidrs                 = ["10.0.1.0/24", "10.0.2.0/24"]
+    availability_zones                  = ["eu-central-1a", "eu-central-1b"]
+    allowed_ports                       = ["80", "443"]
+
+    #Internet-GateWay
+    enable_internet_gateway             = "true"
+    # EIP
+    enable_eip                          = "false"
+}
